ba82c423310d7f9d620505d79fee72834e8901ed
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-07-24 15:15:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,45 +32,63 @@ os.environ['OPENAI_API_KEY'] = st.secrets['openai_api_key']
 # Load Pinecone API key
 api_key = st.secrets["pinecone_api_key"]
 pinecone.init(api_key=api_key, environment='asia-southeast1-gcp-free')
-index_name = 'dbpaseg'
+#index_name = 'db-paseg'
 
-# Function to load embeddings and Pinecone client with RetrievalQA inside
-@st.cache_resource  # Allow output mutation for the Pinecone client
-def load_embeddings_and_pinecone():
-    embeddings = HuggingFaceEmbeddings()
-    docsearch = Pinecone.from_existing_index(index_name, embeddings)
-    chat = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.80)
-    qachain = load_qa_chain(chat, chain_type='stuff')
-    qa = RetrievalQA(combine_documents_chain=qachain, retriever=docsearch.as_retriever())
-    return qa
+# Define Pinecone index name
+index_name = 'db-paseg'
 
-# Create the RetrievalQA object
-qa = load_embeddings_and_pinecone()
+# Define Streamlit app
+st.set_page_config(page_title="PASEG Genie ", page_icon=":coffee:", layout="wide")
 
-condition1 = '\n [organize information: organize text so its easy to read, and bullet points when needed.] \n [tone and voice style: clear sentences, avoid use of complex sentences]'
+# Define username and password
+username = "ppca"
+password = "65326"
 
-st.title("PASEG Genie // Donate a Coffee :coffee:")
+# Define Streamlit sidebar for login
+st.sidebar.title("Login")
+login_username = st.sidebar.text_input("Username")
+login_password = st.sidebar.text_input("Password", type="password")
 
-# Initialize chat history
-#if "messages" not in st.session_state:
-#    st.session_state.messages = []
+# Define Streamlit main page
+st.title("PASEG Genie // for education purpose :coffee:")
+st.markdown("*Chat With The Planning and Schedule Excellence Guide ver. 5.0*", unsafe_allow_html=True)
+st.markdown("---")
 
-# Display chat messages from history on app rerun
-#for message in st.session_state.messages:
-#    with st.chat_message(message["role"]):
-#        st.markdown(message["content"])
+# Define function to check login credentials
+def check_login(username, password):
+    if username == "ppca" and password == "65326":
+        return True
+    else:
+        return False
 
-# Accept user input
-if query := st.chat_input("Enter your query:"):
-    # Add user message to chat history
-   # st.session_state.messages.append({"role": "user", "content": query})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(query)
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        full_response = ""
-        for response in qa.run(query + '\n' + condition1):
-            full_response = response 
-            st.markdown(full_response + "▌")
-   # st.session_state.messages.append({"role": "assistant", "content": full_response})
+# Check login credentials and show query input if login is successful
+if check_login(login_username, login_password):
+    st.write("Login successful!")
+    query = st.text_input("Enter your query:")
+    if query:
+        # Load embeddings and Pinecone client
+        @st.cache_resource
+        def load_embedding():
+            embeddings = HuggingFaceEmbeddings()
+            return embeddings
+
+        embeddings = load_embedding()
+
+        def load_pinecone(embeddings, index_name):
+            docsearch = Pinecone.from_existing_index(index_name, embeddings)
+            return docsearch
+
+        docsearch = load_pinecone(embeddings, index_name)
+
+        # Create the Chat and RetrievalQA objects
+        chat = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.80)
+        qachain = load_qa_chain(chat, chain_type='stuff')
+        qa = RetrievalQA(combine_documents_chain=qachain, retriever=docsearch.as_retriever())
+
+        condition1 = '\n [Generate Response/Text from my data.]  \n [organize information: organize text so its easy to read, and bullet points when needed.] \n [if applicable for the question response, add section: Things to Promote/Things to Avoid and Best Practices, give Examples] \n [tone and voice style: clear sentences, avoid use of complex sentences]'
+
+        # Run the QA system and display the result using Streamlit
+        result = qa.run(query + '\n' + condition1)
+        st.write(result)
+else:
+    st.write("Login failed. Please check your credentials.")
