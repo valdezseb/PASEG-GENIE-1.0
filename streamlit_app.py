9385bdc7f96ccfa911a3eae51764653ced0a4949
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-08-21 10:06:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ pinecone.init(api_key=api_key, environment='asia-southeast1-gcp-free')
 #index_name = 'db-paseg'
 
 # Define Pinecone index name
-index_name = 'db-paseg'
+index_name = 'genie-bok'
 
 # Define Streamlit app
 #st.set_page_config(page_title="PASEG Genie ", page_icon=":coffee:", layout="wide")
