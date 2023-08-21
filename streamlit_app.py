d1025d60021678444f504a9c5f6aab93e20adaee
valdezseb <111101968+valdezseb@users.noreply.github.com>
2023-08-21 10:10:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import RetrievalQA
 
 
-st.set_page_config(page_title="PASEG Genie", page_icon=":coffee:", layout="centered")
-
+#st.set_page_config(page_title="PASEG Genie", page_icon=":coffee:", layout="centered")
+st.set_page_config(page_title="BOK 7th Genie", page_icon=":coffee:", layout="centered")
 # Load Pinecone API key
 api_key = st.secrets["pinecone_api_key"]
 pinecone.init(api_key=api_key, environment='asia-southeast1-gcp-free')
